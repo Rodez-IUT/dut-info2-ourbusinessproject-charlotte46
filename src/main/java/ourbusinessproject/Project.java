3d2dbcff67bf3fd270785a8047563d6191ef1ce1
Charlotte Pont <charlotte.pont@iut-rodez.fr>
2020-01-29 14:29:53 +0100

    fix 1.2.4 classe Project

---
+++ src/main/java/ourbusinessproject/Project.java
@@ -0,0 +1,27 @@
+package ourbusinessproject;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Project {
+
+    @NotEmpty
+    private String title;
+
+    private String description;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
